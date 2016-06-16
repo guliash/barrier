cba011028392ba2630141c3002b581d8f776e6b8
Artem Gilmudinov <guliash92@gmail.com>
2016-06-16 20:34:40 +0300

    removed http-proxy;

--- server/server.js
+++ server/server.js
@@ -1,10 +1,4 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-
-httpProxy.createServer({
-  target:'http://localhost:9003'
-}).listen(8000);
-
+var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
